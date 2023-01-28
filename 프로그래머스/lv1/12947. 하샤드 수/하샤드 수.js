45b2c2f656181340169f1ff7ff5b5f15c3d9f3e1
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-01-28 15:51:24 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,9 +1,5 @@
 function solution(x) {
     let arr = x.toString().split("").map(Number).reduce((a,b) => a+b , 0)
              //정수x를 문자화->배열화->   숫자화  -> 원소들의 합
-    if ( x%arr === 0) { // x를 arr로 나눴을 때 나머지가 0이면 하샤드 수
-        return true;
-    } else {
-        return false;
-    }
+   return ( x%arr === 0)
 }
