75a5281a8bfde6a3aeffbe56c8f4ba6032725e14
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-01-28 14:32:33 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,10 +1,9 @@
 function solution(arr) {
-    let a = [];
-    let min = Math.min(...arr);
-    let answer = arr.filter((i) => i !== min);
-    if (answer.length == 0 ) {
-         a.push(-1);
-        return a
+    let min = Math.min(...arr); //최소값 찾기
+    let answer = arr.filter((i) => i !== min);//최소값만 제거
+    if (answer.length == 0 ) {//빈배열일때 배열 길이는 0
+                                // 배열에 -1 추가
+        return [-1];
         }
     return answer;
 }
